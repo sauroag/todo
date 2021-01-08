d0ec74078cb3fca5d951b0751000e9c393614518
saurabh <yoursaurabh2705@gmail.com>
2021-01-08 09:24:20 +0530

    resolved error for mongodb config and removed validator files

--- src/typings/index.ts
+++ src/typings/index.ts
@@ -2,7 +2,6 @@ import { AppContext } from './app-context';
 import * as Errors from './error';
 import { ErrorPayload } from './error-payload';
 import { ExtendedRequest } from './extended-request';
-import { JwtPayload } from './jwt-payload';
 import { Logger } from './logger';
 import { LooseObject } from './loose-object';
 import { ValidationFailure } from './validation-failure';
@@ -13,7 +12,6 @@ export {
   ErrorPayload,
   Errors,
   ExtendedRequest,
-  JwtPayload,
   Logger,
   LooseObject,
   OtpPayload,
--- src/validators/index.ts
+++
@@ -1,4 +0,0 @@
-import createAccessTokenValidator from './create-access-token-validator';
-import createAccountValidator from './create-account-validator';
-
-export { createAccessTokenValidator, createAccountValidator };
